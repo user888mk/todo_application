463416beb88fd908308b676211a1cce8346645a8
Marcin <marcin.krysiak889@gmail.com>
2022-02-01 19:31:29 +0100

    Service, configured JETTY, frame of web app

---
+++ src/main/java/com/github/user888mk/HelloServlet.java
@@ -0,0 +1,35 @@
+package com.github.user888mk;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Hello word", urlPatterns = {"/api/*"})
+public class HelloServlet extends HttpServlet {
+    private static final String NAME_PARAM = "name";
+    private final Logger logger = LoggerFactory.getLogger(HelloServlet.class);
+
+    private HelloService service;
+
+    //For servlet container
+    @SuppressWarnings("unused")
+    public HelloServlet(HelloServlet service) {
+        this(new HelloService());
+    }
+
+    HelloServlet(HelloService service) {
+        this.service = service;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logger.info("Got request with parameters: " + req.getParameterMap());
+        resp.getWriter().write(service.prepareGreeting(req.getParameter(NAME_PARAM)));
+    }
+}
---
+++ src/test/java/com/github/user888mk/AppTest.java
@@ -0,0 +1,12 @@
+package com.github.user888mk;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void testDemo() throws Exception {
+        assertTrue(true);
+    }
+}
